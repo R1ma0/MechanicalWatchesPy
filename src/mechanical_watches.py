719469b380d927d8ff50e3e4054c45904bd181a2
r1ma0 <cactus-2015@mail.ru>
2023-10-29 00:33:25 +0300

    Added watches cuttoffs

--- src/mechanical_watches.py
+++ src/mechanical_watches.py
@@ -3,7 +3,7 @@ import math
 from pygame.locals import *
 from datetime import datetime
 from preferences import *
-from watches_hand import *
+from watches_line import *
 
 class MechanicalWatches:
 
@@ -22,30 +22,8 @@ class MechanicalWatches:
             self.__bg_image, (WINDOW_WIDTH, WINDOW_HEIGHT)
         )
 
-        self.__watches_hands = (
-            WatchesHand(
-                self.__window,
-                thickness = HOURS_HAND_THICKNESS,
-                degrees_step = HOURS_HAND_DEGREES_STEP,
-                length = HOURS_HAND_LENGTH,
-                color = BLACK_COLOR
-            ),
-            WatchesHand(
-                self.__window,
-                thickness = MINUTES_HAND_THICKNESS,
-                degrees_step = MINUTES_HAND_DEGREES_STEP,
-                length = MINUTES_HAND_LENGTH,
-                color = BLACK_COLOR
-            ),
-            WatchesHand(
-                self.__window,
-                thickness = SECONDS_HAND_THICKNESS,
-                degrees_step = SECONDS_HAND_DEGREES_STEP,
-                length = SECONDS_HAND_LENGTH,
-                color = BLACK_COLOR
-            )
-        )
-        
+        self.__create_watches_arrows()
+
     def run(self) -> None:
         while self.__running:
             self.__current_time = self.__get_current_time()
@@ -61,6 +39,70 @@ class MechanicalWatches:
 
         pygame.quit()
 
+    def __create_watches_arrows(self):
+        self.__watches_hands = (
+            WatchesLine(
+                self.__window,
+                HOURS_HAND_THICKNESS,
+                HOURS_HAND_DEGREES_STEP,
+                HOURS_HAND_COLOR,
+                0,
+                HOURS_HAND_LENGTH
+            ),
+            WatchesLine(
+                self.__window,
+                MINUTES_HAND_THICKNESS,
+                MINUTES_HAND_DEGREES_STEP,
+                MINUTES_HAND_COLOR,
+                0,
+                MINUTES_HAND_LENGTH
+            ),
+            WatchesLine(
+                self.__window,
+                SECONDS_HAND_THICKNESS,
+                SECONDS_HAND_DEGREES_STEP,
+                SECONDS_HAND_COLOR,
+                0,
+                SECONDS_HAND_LENGTH
+            )
+        )
+
+        self.__sec_min_cuttoffs = []
+
+        for ct in range(0, 60, 1): # ct - cuttoff time value
+            if ct % 5 == 0:
+                continue
+
+            self.__sec_min_cuttoffs.append(
+                WatchesLine(
+                    self.__window,
+                    SEC_MIN_CUTTOFFS_THICKNESS,
+                    SECONDS_HAND_DEGREES_STEP,
+                    SEC_MIN_CUTTOFFS_COLOR,
+                    SEC_MIN_CUTTOFFS_START_POS,
+                    SEC_MIN_CUTTOFFS_END_POS,
+                    ct
+                )   
+            )
+
+        self.__hours_cuttoffs = []
+
+        for ct in range(0, 60, 1):
+            if ct % 5 != 0:
+                continue
+
+            self.__hours_cuttoffs.append(
+                WatchesLine(
+                    self.__window,
+                    HOURS_CUTTOFFS_THICKNESS,
+                    HOURS_HAND_DEGREES_STEP,
+                    HOURS_CUTTOFFS_COLOR,
+                    HOURS_CUTTOFFS_START_POS,
+                    HOURS_CUTTOFFS_END_POS,
+                    ct
+                )   
+            )
+
     def __process_events(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
@@ -72,32 +114,42 @@ class MechanicalWatches:
 
     def __draw_objects(self) -> None:
         # Watches background
+
         pygame.draw.circle(
             self.__window, 
-            WHITE_COLOR, 
+            WATCHES_BG_COLOR, 
             (WINDOW_HALF_WIDTH, WINDOW_HALF_HEIGHT), 
             WATCHES_BG_RADIUS
         )
 
         # Watches edge
+
         pygame.draw.circle(
             self.__window, 
-            BLACK_COLOR, 
+            WATCHES_EDGE_COLOR, 
             (WINDOW_HALF_WIDTH, WINDOW_HALF_HEIGHT), 
             WATCHES_BG_RADIUS,
             WATCHES_BG_EDGE_THICKNESS
         )
 
-        # TODO: Time cuttoffs
+        # Cuttoffs
+
+        for sec_min_cuttoff in self.__sec_min_cuttoffs:
+            sec_min_cuttoff.draw()
+
+        for hour_cuttoff in self.__hours_cuttoffs:
+            hour_cuttoff.draw()
 
         # Watches hands
+
         for tci in range(len(self.__current_time)):
             self.__watches_hands[tci].draw(self.__current_time[tci])
 
         # Watches middle
+
         pygame.draw.circle(
             self.__window,
-            BLACK_COLOR,
+            WATCHES_MIDDLE_COLOR,
             (WINDOW_HALF_WIDTH, WINDOW_HALF_HEIGHT),
             WATCHES_MIDDLE_CIRCLE_RADIUS
         )
